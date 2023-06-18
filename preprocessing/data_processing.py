da6a456a31a3433866259a7283646d82525ebb06
SAint7579 <vishwa20@oulook.com>
2023-06-18 22:56:04 +0200

    Added VGG16 and ResNet

--- preprocessing/data_processing.py
+++ preprocessing/data_processing.py
@@ -4,10 +4,78 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 from tensorflow.keras import layers
 from tensorflow import keras
+import os
+from sklearn.model_selection import train_test_split
+import shutil
+import cv2
+from tqdm import tqdm
+import imutils
+
+def crop_imgs(set_name, add_pixels_value=0):
+    """
+    Finds the extreme points on the image and crops the rectangular out of them
+    """
+
+    ## Get the shape
+    shape = set_name[0].shape
+    set_new = []
+    for img in set_name:
+
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+        # threshold the image, then perform a series of erosions +
+        # dilations to remove any small regions of noise
+        thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.erode(thresh, None, iterations=2)
+        thresh = cv2.dilate(thresh, None, iterations=2)
+
+        # find contours in thresholded image, then grab the largest one
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        c = max(cnts, key=cv2.contourArea)
+
+        # find the extreme points
+        extLeft = tuple(c[c[:, :, 0].argmin()][0])
+        extRight = tuple(c[c[:, :, 0].argmax()][0])
+        extTop = tuple(c[c[:, :, 1].argmin()][0])
+        extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+        ADD_PIXELS = add_pixels_value
+        new_img = img[extTop[1]-ADD_PIXELS:extBot[1]+ADD_PIXELS, extLeft[0]-ADD_PIXELS:extRight[0]+ADD_PIXELS].copy()
+        new_img = cv2.resize(new_img, shape[:2])
+        set_new.append(new_img)
+
+
+    return np.array(set_new)
+
+
+def load_data_tonumpy(dir_path, img_size=(100,100)):
+    """
+    Load resized images as np.arrays to workspace
+    """
+    X = []
+    y = []
+    i = 0
+    labels = dict()
+    for path in tqdm(sorted(os.listdir(dir_path))):
+        if not path.startswith('.'):
+            labels[i] = path
+            for file in os.listdir(dir_path + path):
+                if not file.startswith('.'):
+                    img = cv2.imread(dir_path + path + '/' + file)
+                    img = cv2.resize(img, img_size)
+                    X.append(img)
+                    y.append(i)
+            i += 1
+    X = np.array(X)
+    y = np.array(y)
+    print(f'{len(X)} images loaded from {dir_path} directory.')
+    return X, y, labels
 
 
 
-def create_train_test_dataset(train_directory, test_directory):
+def create_train_test_dataset_from_generators(train_directory, test_directory):
 
     img_height, img_width = 224,224
     batch_size = 32
